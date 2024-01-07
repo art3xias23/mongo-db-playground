a17c738613fba725c3f1bc5ebc3f2bdae40549f1
Tino <Konstantin.v.milchev@gmail.com>
2024-01-07 20:56:05 +0000

    getting by object id

--- NodeApp/app.js
+++ NodeApp/app.js
@@ -1,36 +1,51 @@
 const express = require("express");
-const {connectToDb, getDb} = require('./db')
+const { connectToDb, getDb } = require("./db");
+const { ObjectId } = require("mongodb");
 
 // install app & middleware
 const app = express();
 
-let db
+let db;
 connectToDb((err) => {
-  if (!err){
+  if (!err) {
     app.listen("3000", () => {
       console.log("app listening on port 3000");
     });
 
-    db = getDb()
+    db = getDb();
   }
-}) 
-
+});
 
 //routes
 
 app.get("/books", (req, res) => {
-  let books =[]
+  let books = [];
 
-db.collection('books')
-  .find()
-  .sort({author:1}) // find returns a cursor that points to a collection. 
-  .forEach(book => books.push(book))
-  .then(() => {
-    res.status(200).json(books)
-  })
-  .catch((err) => {
-res.status(500).json({error: 'Could not fetch the documents', details: err.message})
-  })
-          // The cursor exposes methods like toArray and forEach which could be used on the collection
+  db.collection("books")
+    .find()
+    .sort({ author: 1 }) // find returns a cursor that points to a collection.
+    .forEach((book) => books.push(book))
+    .then(() => {
+      res.status(200).json(books);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: "Could not fetch the documents", details: err.message });
+    });
+  // The cursor exposes methods like toArray and forEach which could be used on the collection
+});
 
+app.get("/books/:id", (req, res) => {
+  console.log(req.params.id)
+  db.collection("books")
+    .findOne({ _id: new ObjectId(req.params.id) })
+    .then((doc) => {
+      res.status(200).json(doc);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ error: "Could not fetch the documents", details: err.message });
+    });
 });
