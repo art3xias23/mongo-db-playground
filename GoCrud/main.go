e5baa99f19766b4ec980168eec4cac36543fab1f
Tino <Konstantin.v.milchev@gmail.com>
2024-04-25 08:51:09 +0100

    added example data

--- GoCrud/main.go
+++ GoCrud/main.go
@@ -12,18 +12,32 @@ func main(){
 	
 	http.HandleFunc("/", getHome)
 
-	http.ListenAndServe(":3333", nil)
+	http.ListenAndServe(":3000", nil)
+}
+
+type Animal struct{
+	Type string
+	Name string
 }
 
 func getHome(w http.ResponseWriter, r *http.Request){
 var tmplFile = "templates/layout.html"
+items:= []Animal{
+	{Type: "Dog", Name: "Rob"},
+	{Type: "Cat", Name: "Mandy"},
+}
+ data := struct{
+	 Items []Animal
+ }{
+	 Items: items}
+
 tmpl, err:= template.ParseFiles(tmplFile)
 
 if err!=nil{
 	panic(err)
 }
 
-err = tmpl.Execute(w, nil)
+err = tmpl.Execute(w, data)
 if err!=nil{
 	panic(err)
 }
